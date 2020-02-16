c184541c52d0f2d7dfa0b307789d21356d970715
Abduragmaan Frank <abduragmaanfrank@gmail.com>
2020-02-16 21:46:34 +0200

    second commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("testing my version control");
+        System.out.println("making a random change");
+        System.out.println("making 2nd change");
     }
 }
