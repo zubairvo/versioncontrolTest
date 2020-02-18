add3ab6a41d393291699fab6a9736468a0de82bf
Abduragmaan Frank <abduragmaanfrank@gmail.com>
2020-02-18 21:30:31 +0200

    After branches created

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("testing my version control");
         System.out.println("making a random change");
         System.out.println("making 2nd change!!!");
+        System.out.println("made this change in beta!!!");
     }
 }
